3a622b4957b4c574548268d56f4adb6d55318e43
rew <rew@tencorvids.com>
2024-08-12 10:05:41 -0400

    Geist mono nerd font

--- home/fonts.nix
+++ home/fonts.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   home.packages = [
     pkgs.nerdfonts
-    (pkgs.nerdfonts.override {fonts = ["IBMPlexMono"];})
+    (pkgs.nerdfonts.override {fonts = ["IBMPlexMono" "GeistMono"];})
     pkgs.twemoji-color-font
     pkgs.geist-font
   ];
