96bc0d112b35a72e867f90873517db32523e0975
rew <rew@tencorvids.com>
2024-08-14 09:47:20 -0400

    Wayland session variables

--- home/wm/hyprland/variables.nix
+++ home/wm/hyprland/variables.nix
@@ -2,5 +2,9 @@
   home.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     WLR_NO_HARDWARE_CURSORS = "1";
+    MOZ_ENABLE_WAYLAND = 1;
+    XDG_SESSION_TYPE = "wayland";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DESKTOP = "Hyprland";
   };
 }
