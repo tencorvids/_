158074a6b2a94b32332b847016a36ef123b93670
rew <rew@tencorvids.com>
2024-08-23 11:37:10 -0400

    Poetry python package manager

--- home/default.nix
+++ home/default.nix
@@ -15,7 +15,6 @@
     stateVersion = "24.05";
   };
 
-  # Let Home Manager install and manage itself.
   programs = {
     home-manager.enable = true;
   };
--- home/shell/langs/python.nix
+++ home/shell/langs/python.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
+    poetry
     python3
     python3Packages.pip
     python3Packages.virtualenv
