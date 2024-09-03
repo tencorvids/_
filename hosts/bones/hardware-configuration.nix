09cfc1305ad265b18302a7962b12aec168f094a6
rew <rew@tencorvids.com>
2024-09-03 15:16:23 -0400

    `bones` new hardware configuration

--- hosts/bones/hardware-configuration.nix
+++ hosts/bones/hardware-configuration.nix
@@ -8,18 +8,18 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b0ea52df-2302-48c5-b096-f66c3d6d84da";
+    { device = "/dev/disk/by-uuid/841c3254-4bac-409f-8ecb-cc0dd14665a6";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1B8A-151F";
+    { device = "/dev/disk/by-uuid/A9A8-CE84";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
