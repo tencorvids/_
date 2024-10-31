7d9fe5a3133384e24e4b0170b5c9d5fa801aace9
rew <rew@tencorvids.com>
2024-10-31 17:32:06 -0400

    Update nix.package

--- modules/os.nix
+++ modules/os.nix
@@ -10,7 +10,7 @@
     };
   };
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = "experimental-features = nix-command flakes";
     settings = {
       auto-optimise-store = true;
